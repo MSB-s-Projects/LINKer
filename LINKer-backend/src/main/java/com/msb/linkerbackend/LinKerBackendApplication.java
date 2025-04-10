a428635633ec8c0ecbe867989fa120725224f007
Manobal-Singh-Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2025-04-10 14:02:59 +0530

    initialized backend in spring boot

---
+++ LINKer-backend/src/main/java/com/msb/linkerbackend/LinKerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.msb.linkerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinKerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinKerBackendApplication.class, args);
+    }
+
+}
---
+++ LINKer-backend/src/test/java/com/msb/linkerbackend/LinKerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msb.linkerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinKerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
