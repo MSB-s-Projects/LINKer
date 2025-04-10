328e4565016b33ebd07037a499af904549071bde
Manobal-Singh-Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2025-04-10 16:30:11 +0530

    add initial entity models and database configuration

---
+++ LINKer-backend/src/main/java/com/msb/linkerbackend/models/User.java
@@ -0,0 +1,22 @@
+package com.msb.linkerbackend.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String role = "USER";
+
+    @OneToMany(mappedBy = "user")
+    private List<UrlMapping> urlMappings;
+}
